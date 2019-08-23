d449cf3b6916a39ad7c1566d01a8867634647b62
Josh Kirschenbaum <JoshKBaum@gmail.com>
2019-08-23 01:00:20 -0500

    building layout

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modules: [1, 2, 3, 4, 5, 6],
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>Privacy Please</Text>
+        {this.renderModules()}
+      </View>
+    );
+  }
+
+  renderModules() {
+    return this.state.modules.map(item => {
+      return (
+        <View key={item} style={styles.module}>
+          <Text>{item}</Text>
+        </View>
+      );
+    });
+  }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15,
+    marginTop: 20,
+  },
+  module: {
+    backgroundColor: 'darkcyan',
+    height: 75,
+    width: 300,
+    marginBottom: 10,
   },
 });
